e4df13eff7260353afeff8318a9bee22b5e767b3
Morten Teinum <morten.teinum@gmail.com>
2024-02-10 21:32:14 +0100

    Add project files.

---
+++ MainForm.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace SiusDataStartList
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.buttonConvert = new System.Windows.Forms.Button();
+            this.buttonExport = new System.Windows.Forms.Button();
+            this.listBoxFiles = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // buttonConvert
+            // 
+            this.buttonConvert.Location = new System.Drawing.Point(268, 386);
+            this.buttonConvert.Name = "buttonConvert";
+            this.buttonConvert.Size = new System.Drawing.Size(75, 23);
+            this.buttonConvert.TabIndex = 0;
+            this.buttonConvert.Text = "&Convert";
+            this.buttonConvert.UseVisualStyleBackColor = true;
+            this.buttonConvert.Click += new System.EventHandler(this.buttonConvert_Click);
+            // 
+            // buttonExport
+            // 
+            this.buttonExport.Location = new System.Drawing.Point(349, 386);
+            this.buttonExport.Name = "buttonExport";
+            this.buttonExport.Size = new System.Drawing.Size(75, 23);
+            this.buttonExport.TabIndex = 1;
+            this.buttonExport.Text = "&Export";
+            this.buttonExport.UseVisualStyleBackColor = true;
+            this.buttonExport.Click += new System.EventHandler(this.buttonExport_Click);
+            // 
+            // listBoxFiles
+            // 
+            this.listBoxFiles.DisplayMember = "Name";
+            this.listBoxFiles.FormattingEnabled = true;
+            this.listBoxFiles.Location = new System.Drawing.Point(12, 12);
+            this.listBoxFiles.Name = "listBoxFiles";
+            this.listBoxFiles.Size = new System.Drawing.Size(412, 368);
+            this.listBoxFiles.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(441, 427);
+            this.Controls.Add(this.listBoxFiles);
+            this.Controls.Add(this.buttonExport);
+            this.Controls.Add(this.buttonConvert);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "SiusData StartList";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonConvert;
+        private System.Windows.Forms.Button buttonExport;
+        private System.Windows.Forms.ListBox listBoxFiles;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SiusDataStartList
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private readonly string SiusDataFolder = ConfigurationManager.AppSettings["SiusDataFolder"];
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var directory = new DirectoryInfo(SiusDataFolder);
+
+            foreach (var file in directory.GetFiles("*_stl.csv"))
+            {
+                this.listBoxFiles.Items.Add(file);
+            }
+        }
+
+        private string[] GetFileContent(FileInfo fi)
+        {
+            return File.ReadAllLines(fi.FullName, System.Text.Encoding.GetEncoding(1252));
+        }
+
+        private void buttonConvert_Click(object sender, EventArgs e)
+        {
+            var x = (FileInfo)listBoxFiles.SelectedItem;
+
+            var toWrite = new List<string>();
+
+            foreach (var line in GetFileContent(x))
+            {
+                var parts = line.Split(';');
+
+                var name = parts[2];
+                var firstName = parts[3];
+
+                if (name.StartsWith(firstName))
+                {
+                    parts[2] = name.Substring(firstName.Length + 1);
+
+                    var newLine = string.Join(";", parts);
+
+                    toWrite.Add(newLine);
+                }
+                else
+                {
+                    toWrite.Add(line);
+                }
+            }
+
+            File.WriteAllLines(x.FullName, toWrite, System.Text.Encoding.GetEncoding(1252));
+
+            MessageBox.Show("Done!");
+        }
+
+        private void buttonExport_Click(object sender, EventArgs e)
+        {
+            var x = (FileInfo)listBoxFiles.SelectedItem;
+
+            var sb = new StringBuilder();
+            sb.AppendLine("Start Number;Name;FirstName;DisplayName;Nation;Team;TargetNumber;Relay");
+
+            foreach (var line in GetFileContent(x))
+            {
+                var parts = line.Split(';');
+                var startNumber = parts[1];
+                var name = parts[2];
+                var FirstName = parts[3];
+                var DisplayName = parts[4];
+                var Nation = parts[5];
+                var Team = parts[8];
+                var TargetNumber = parts[10];
+                var Relay = parts[11];
+
+                var shooter =
+                    $"{startNumber};{name};{FirstName};{DisplayName};{Nation};{Team};{TargetNumber};{Relay}";
+
+                sb.AppendLine(shooter);
+            }
+
+            var content = sb.ToString();
+            Clipboard.SetText(content, TextDataFormat.UnicodeText);
+        }
+    }
+}
